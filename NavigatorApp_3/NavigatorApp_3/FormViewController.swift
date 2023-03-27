c89c85c62daaa8261aea75f6c97f05ec2a50bda7
KetulPatel14 <ketul9744@gmail.com>
2023-03-27 12:10:22 -0400

    create a new 2 file
    formViewcontroller and infoviewcontroller

---
+++ NavigatorApp_3/NavigatorApp_3/FormViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FormViewController.swift
+//  NavigatorApp_3
+//
+//  Created by Canadore Student on 2023-03-27.
+//  Copyright © 2023 Canadore Student. All rights reserved.
+//
+
+import UIKit
+
+class FormViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
